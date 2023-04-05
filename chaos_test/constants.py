cc1367cf4c5bbcf36b2d2f0ff1ce9614b1f131cc
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-04-04 18:00:18 -0700

    Add ServiceStatus enum
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- chaos_test/constants.py
+++ chaos_test/constants.py
@@ -6,3 +6,22 @@ RECEIVER_KILL_KEY = "kill_node"
 class KillOptions(str, Enum):
     KILL = "kill"
     SPARE = "spare"
+
+
+class ServiceStatus(str, Enum):
+    ROLLOUT_INITIATED = "Rollout initiated"
+    ROLLBACK_INITIATED = "Rollback initiated"
+    ROLLOUT_UPDATED = "Rollout updated"
+    CREATE_INITIATED = "Create initiated"
+    RESTART_INITIATED = "Restart initiated"
+    TERMINATE_INITIATED = "Terminate initiated"
+    UPDATE_INITIATED = "Update initiated"
+    RUNNING = "Running"
+    UNHEALTHY = "Unhealthy"
+    SYSTEM_FAILURE = "System failure"
+    STARTING = "Starting"
+    TERMINATED = "Terminated"
+    TERMINATING = "Terminating"
+    UPDATING = "Updating"
+    ROLLING_OUT = "Rolling out"
+    ROLLINGBACK = "Rolling back"
