59cfff50e2870a10b66c8f9e23449d62a3e9bde7
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-07-25 15:56:05 -0700

    Update locustfile for qps test
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- microbenchmarks/locust_file.py
+++
@@ -1,9 +0,0 @@
-from locust import HttpUser, task, constant
-
-
-class ConstantUser(HttpUser):
-    wait_time = constant(0)
-
-    @task
-    def hello_world(self):
-        self.client.get("/")
---
+++ microbenchmarks/qps_test_locustfile.py
@@ -0,0 +1,34 @@
+from locust import FastHttpUser, task, constant, events
+
+
+@events.init_command_line_parser.add_listener
+def _(parser):
+    parser.add_argument(
+        "-p", "--payload-size", type=int, default=0, help="Request body size."
+    )
+    parser.add_argument(
+        "-t",
+        "--bearer-token",
+        type=str,
+        default="",
+        help="Bearer token of Anyscale service.",
+    )
+
+
+@events.test_start.add_listener
+def _(environment, **kw):
+    payload_size: int = environment.parsed_options.payload_size
+    environment.payload = "0" * payload_size
+
+
+class ConstantUser(FastHttpUser):
+    wait_time = constant(0)
+
+    @task
+    def hello_world(self):
+        bearer_token = self.bearer_token.parsed_options.bearer_token
+        self.client.post(
+            "/",
+            json=self.environment.payload,
+            headers={"Authorization": f"Bearer {bearer_token}"},
+        )
