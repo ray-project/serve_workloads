4f723ffff5fab78c2a5b32931a574c830a143b52
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-09-01 16:26:22 -0700

    Add echo statement
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- microbenchmarks/run_experiments.sh
+++ microbenchmarks/run_experiments.sh
@@ -5,6 +5,7 @@ for payload_size in 0 1000000 10000000
 do
     for num_users in 500 1000 2000 3000 3500 4000 6000 8000
     do
+        echo "Running with payload size $payload_size and $num_users users."
         python locust_runner.py -f qps_test_locustfile.py -u $num_users -r 100 -p $payload_size --host $HOST -b $BEARER_TOKEN
     done
 done
