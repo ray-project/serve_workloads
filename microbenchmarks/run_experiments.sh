c3f2761e38729e617e6325c9fed06f1dee51e504
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-09-01 16:23:50 -0700

    Create shell script to run experiments
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

---
+++ microbenchmarks/run_experiments.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+HOST=''
+BEARER_TOKEN=''
+for payload_size in 0 1000000 10000000
+do
+    for num_users in 500 1000 2000 3000 3500 4000 6000 8000
+    do
+        python locust_runner.py -f qps_test_locustfile.py -u $num_users -r 100 -p $payload_size --host $HOST -b $BEARER_TOKEN
+    done
+done
