4655c597bc6bf9ea2d87abf35accf1961770d47a
Shreyas Krishnaswamy <shrekris@anyscale.com>
2023-09-09 16:05:12 -0700

    Update client logic
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- math_solver/constant_load.py
+++ math_solver/constant_load.py
@@ -8,8 +8,6 @@ CLI command:
         -f constant_load.py
 """
 
-import json
-import base64
 from locust import FastHttpUser, task, constant, events
 
 
@@ -27,15 +25,6 @@ def _(parser):
     )
 
 
-@events.test_start.add_listener
-def _(environment, **kw):
-    with open(IMAGE_FILE, "rb") as f:
-        image_bytes = f.read()
-    
-    image_bytes_utf8: str = base64.b64encode(image_bytes).decode("utf8")
-    environment.payload = json.dumps({"image_bytes_utf8": image_bytes_utf8})
-
-
 class ConstantUser(FastHttpUser):
     wait_time = constant(0)
     network_timeout = None
@@ -43,12 +32,11 @@ class ConstantUser(FastHttpUser):
 
     @task
     def hello_world(self):
-        bearer_token = self.environment.parsed_options.bearer_token
-        self.client.post(
-            "/",
-            data=self.environment.payload,
-            headers={
-                "Authorization": f"Bearer {bearer_token}",
-                "Content-type": "application/json",
-            },
-        )
+        with open(IMAGE_FILE, "rb") as f:
+            files = {"image_file": f}
+            bearer_token = self.environment.parsed_options.bearer_token
+            self.client.post(
+                "/",
+                files=files,
+                headers={"Authorization": f"Bearer {bearer_token}"},
+            )
