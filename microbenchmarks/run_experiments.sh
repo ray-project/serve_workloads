53e4a51f8d0bd6bd2104961f5bec615b52c33d7d
Shreyas Krishnaswamy <shrekris@anyscale.com>
2024-01-04 16:00:26 -0800

    Add a time argument to the shell script
    Signed-off-by: Shreyas Krishnaswamy <shrekris@anyscale.com>

--- microbenchmarks/run_experiments.sh
+++ microbenchmarks/run_experiments.sh
@@ -6,6 +6,6 @@ do
     for num_users in 500 1000 2000 3000 3500 4000 6000 8000
     do
         echo "Running with payload size $payload_size and $num_users users."
-        python locust_runner.py -f qps_test_locustfile.py -u $num_users -r 100 -p $payload_size --host $HOST -b $BEARER_TOKEN
+        python locust_runner.py -f qps_test_locustfile.py -u $num_users -r 100 -p $payload_size --host $HOST -b $BEARER_TOKEN -t 5m
     done
 done
